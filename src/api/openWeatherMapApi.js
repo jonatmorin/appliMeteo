efe6c530cf882490123113e9646981b5391d2d4a
jonatmorin <jonatmorin@gmail.com>
2022-02-14 07:25:33 -0500

    bug/#1 - Refonte des routes vers le nouvel API (openWeatherMap)

--- src/api/openWeatherMapApi.js
+++ src/api/openWeatherMapApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_URL = "https://api.openweathermap.org/data/2.5/weather/";
+const API_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "5d08576bf748affb068993b10407670e";
 
 export const getCurrentWeather = async function (lat, lon) {
     let response = await axios.get(
-        API_URL,
+        `${API_URL}/weather`,
         {
             params: {
                 lat: lat,
@@ -14,5 +14,26 @@ export const getCurrentWeather = async function (lat, lon) {
                 units: "metric"
             }
         })
-    return response;
+    if (response.status !== 200) {
+        throw new Error("Unable to get weather");
+    }
+    return response.data;
+}
+
+export const getForecast5Days = async function (lat, lon) {
+    let response = await axios.get(
+        `${API_URL}/onecall`,
+        {
+            params: {
+                lat: lat,
+                lon: lon,
+                appid: API_KEY,
+                units: "metric"
+            }
+        }
+    )
+    if (response.status !== 200) {
+        throw new Error("Unable to get forecast")
+    }
+    return response.data.daily;
 }
--- src/location.vue
+++ src/location.vue
@@ -1,42 +1,28 @@
 <template>
   <div>
-
-    <!--Ville-->
-      <h2 class="title is-4 has-text-grey-dark has-text-centered">{{ville}}</h2>
-    <!--/Ville-->
-
-    <!--Condition actuelle-->
+    <h2 class="title is-4 has-text-grey-dark has-text-centered">{{ville}}</h2>
     <div class="section is-small has-background-info-dark m-5">
-
-<!--        <h3 class="title is-5 has-text-white">Conditions actuelles en date du {{temp.ob_time}}</h3>-->
       <h3 class="title is-5 has-text-white">Conditions actuelles</h3>
-
-
       <div>{{mercure}} &#176; C, ressentie {{temperatureRessentie}} &#176; C</div>
       <div class="description">
-        <img :src="`http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`">
-        {{currentWeather.description}}
+        <img :src="`http://openweathermap.org/img/wn/${weatherDecription.icon}@2x.png`">
+        {{weatherDecription.description}}
       </div>
     </div>
-    <!--/Condition actuelle-->
   </div>
 </template>
 
 <script>
 
-//import axios from "axios";
 import {getCurrentWeather} from "./api/openWeatherMapApi";
 
 export default {
-
-
-
   name: "location",
-
   data: function () {
     return {
+      currentWeatherData: [],
       ville: '',
-      currentWeather: [],
+      weatherDecription: [],
       mercure: '',
       temperatureRessentie: '',
       coordinatesX: window.localStorage.getItem('navpositionX'),
@@ -45,46 +31,16 @@ export default {
   },
 
   methods: {
-    getWeather: async function () {
-      let weather = getCurrentWeather(this.coordinatesX,this.coordinatesY);
-      this.mercure = weather.data.main.temp;
-      this.temperatureRessentie = weather.data.main.feels_like;
-      this.currentWeather = weather.data.weather[0];
-      this.ville = weather.data.name;
-    },
-
-
-
-/*    getCurrentWeather: async function () {
-      const response = await axios.get(
-          "https://api.openweathermap.org/data/2.5/weather/",
-          {
-            params: {
-              lat: this.coordinatesX,
-              lon: this.coordinatesY,
-              appid: "5d08576bf748affb068993b10407670e",
-              units: "metric"
-            }
-          }
-      )
-      this.mercure = response.data.main.temp;
-      this.temperatureRessentie = response.data.main.feels_like;
-      this.currentWeather = response.data.weather[0];
-      this.ville = response.data.name;
-    },*/
   },
 
-  created:
-    function () {
-      this.getWeather();
-    },
-
-
+  async created() {
+    this.currentWeatherData = await getCurrentWeather(this.coordinatesX,this.coordinatesY);
+    this.mercure = this.currentWeatherData.main.temp;
+    this.temperatureRessentie = this.currentWeatherData.main.feels_like;
+    this.weatherDecription = this.currentWeatherData.weather[0];
+    this.ville = this.currentWeatherData.name;
+  }
 }
-
-
-
-
 </script>
 
 <style scoped>
